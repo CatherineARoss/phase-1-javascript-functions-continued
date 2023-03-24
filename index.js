020fe4f0287260e8963746b380eb3be5bd799ea1
Catherine Ross <fibervibes@gmail.com>
2023-03-23 23:19:11 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity === "bathe my dog") {
+        return(`This Saturday, I want to ${activity}!`);
+    } else 
+        return(`This Saturday, I want to roller-skate!`)
+
+} 
+    
+function mondayWork(chore) {
+    if (chore === "work from home") {
+        return(`This Monday, I will ${chore}.`);
+    } else 
+        return(`This Monday, I will go to the office.`)
+}
+
+function wrapAdjective(result = `*`) {
+   return function (emphatic = `a hard worker`) {
+    return `You are ${result}${emphatic}${result}!`
+   };
+}
+
